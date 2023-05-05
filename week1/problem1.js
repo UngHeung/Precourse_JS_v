8ba74f87a3ef6cd3e20bfc6f5f0b51af01e90c9b
UngHeung <t01047377477@gmail.com>
2023-05-05 19:42:07 +0900

    feat : resolve first problem, test case check

--- week1/problem1.js
+++ week1/problem1.js
@@ -1,10 +1,68 @@
-import TestPr1 from "./test.js";
+import { TestPr1 } from "./test.js";
 
 const scoreBoard = document.getElementById("score_board");
 
+// 문제 입력 시작
+// function solution(pobi, crong) {
+//     return null;
+// }
+
 function solution(pobi, crong) {
-    return 2;
+    const pobiPage = getMax(pobi);
+    const crongPage = getMax(crong);
+
+    if (!checkValid(pobi) || !checkValid(crong)) {
+        return -1;
+    }
+
+    if (pobiPage > crongPage) {
+        return 1;
+    } else if (crongPage > pobiPage) {
+        return 2;
+    } else if (pobiPage === crongPage) {
+        return 0;
+    } else {
+        return -1;
+    }
 }
 
+function getMax(page) {
+    const leftPage = page[0];
+    const rightPage = page[1];
+    const leftMax = Math.max(addition(leftPage), multiplication(leftPage));
+    const rightMax = Math.max(addition(rightPage), multiplication(rightPage));
+
+    return Math.max(leftMax, rightMax);
+}
+
+function addition(num) {
+    const numToString = String(num);
+    const result = [...numToString].reduce((a, b) => a + b * 1, 0);
+    return result;
+}
+
+function multiplication(num) {
+    const numToString = String(num);
+    const result = [...numToString].reduce((a, b) => a * b * 1, 1);
+    return result;
+}
+
+function checkValid([leftPage, rightPage]) {
+    if (!(leftPage < rightPage)) {
+        return false;
+    } else if (rightPage - leftPage !== 1) {
+        return false;
+    }
+
+    return true;
+}
+
+// 문제 입력 종료
+
 const test = new TestPr1(solution);
-scoreBoard.innerHTML = test.printResult();
+scoreBoard.innerHTML = `
+    <li>
+        <h3> 1번 문제 </h3>
+        ${test.printResult()}
+    </li>
+`;
--- week1/test.js
+++ week1/test.js
@@ -1,4 +1,4 @@
-export default class TestPr1 {
+class TestPr1 {
     constructor(solution) {
         this.solution = solution;
         this.checkScore();
@@ -14,32 +14,282 @@ export default class TestPr1 {
 
     /** 점수 확인 */
     checkScore = () => {
+        // result case 1, 2, 3
         const result = [0, 1, -1];
         const [pobi, crong] = [
             [
+                // pobi test case 1, 2, 3
                 [97, 98],
-                [197, 198],
-            ],
-            [
                 [131, 132],
-                [211, 212],
+                [99, 102],
             ],
             [
-                [99, 102],
+                // crong test case 1, 2, 3
+                [197, 198],
+                [211, 212],
                 [211, 212],
             ],
         ];
 
         let count = 0;
-        let score = (count / result.length) * 100;
 
         result.map((_, i) => (this.solution(pobi[i], crong[i]) === result[i] ? count++ : 0));
+
+        let score = ~~((count / result.length) * 100);
+
         this.setResult(
             `
                 테스트 케이스 개수: ${result.length}<br>
                 맞춘 개수: ${count}<br>
-                점수: ${score}<br>
+                점수: ${score}점<br>
             `
         );
     };
 }
+
+class TestPr2 {
+    constructor(solution) {
+        this.solution = solution;
+        this.checkScore();
+    }
+
+    setResult = (result) => {
+        this.result = result;
+    };
+
+    printResult = () => {
+        return this.result;
+    };
+
+    /** 점수 확인 */
+    checkScore = () => {
+        // result case 1, 2, 3
+        const result = [0, 1, -1];
+        const [pobi, crong] = [
+            [
+                // pobi test case 1, 2, 3
+                [97, 98],
+                [131, 132],
+                [99, 102],
+            ],
+            [
+                // crong test case 1, 2, 3
+                [197, 198],
+                [211, 212],
+                [211, 212],
+            ],
+        ];
+
+        let count = 0;
+
+        result.map((_, i) => (this.solution(pobi[i], crong[i]) === result[i] ? count++ : 0));
+
+        let score = ~~((count / result.length) * 100);
+
+        this.setResult(
+            `
+                테스트 케이스 개수: ${result.length}<br>
+                맞춘 개수: ${count}<br>
+                점수: ${score}점<br>
+            `
+        );
+    };
+}
+
+class TestPr3 {
+    constructor(solution) {
+        this.solution = solution;
+        this.checkScore();
+    }
+
+    setResult = (result) => {
+        this.result = result;
+    };
+
+    printResult = () => {
+        return this.result;
+    };
+
+    /** 점수 확인 */
+    checkScore = () => {
+        // result case 1, 2, 3
+        const result = [0, 1, -1];
+        const [pobi, crong] = [
+            [
+                // pobi test case 1, 2, 3
+                [97, 98],
+                [131, 132],
+                [99, 102],
+            ],
+            [
+                // crong test case 1, 2, 3
+                [197, 198],
+                [211, 212],
+                [211, 212],
+            ],
+        ];
+
+        let count = 0;
+
+        result.map((_, i) => (this.solution(pobi[i], crong[i]) === result[i] ? count++ : 0));
+
+        let score = ~~((count / result.length) * 100);
+
+        this.setResult(
+            `
+                테스트 케이스 개수: ${result.length}<br>
+                맞춘 개수: ${count}<br>
+                점수: ${score}점<br>
+            `
+        );
+    };
+}
+
+class TestPr4 {
+    constructor(solution) {
+        this.solution = solution;
+        this.checkScore();
+    }
+
+    setResult = (result) => {
+        this.result = result;
+    };
+
+    printResult = () => {
+        return this.result;
+    };
+
+    /** 점수 확인 */
+    checkScore = () => {
+        // result case 1, 2, 3
+        const result = [0, 1, -1];
+        const [pobi, crong] = [
+            [
+                // pobi test case 1, 2, 3
+                [97, 98],
+                [131, 132],
+                [99, 102],
+            ],
+            [
+                // crong test case 1, 2, 3
+                [197, 198],
+                [211, 212],
+                [211, 212],
+            ],
+        ];
+
+        let count = 0;
+
+        result.map((_, i) => (this.solution(pobi[i], crong[i]) === result[i] ? count++ : 0));
+
+        let score = ~~((count / result.length) * 100);
+
+        this.setResult(
+            `
+                테스트 케이스 개수: ${result.length}<br>
+                맞춘 개수: ${count}<br>
+                점수: ${score}점<br>
+            `
+        );
+    };
+}
+
+class TestPr5 {
+    constructor(solution) {
+        this.solution = solution;
+        this.checkScore();
+    }
+
+    setResult = (result) => {
+        this.result = result;
+    };
+
+    printResult = () => {
+        return this.result;
+    };
+
+    /** 점수 확인 */
+    checkScore = () => {
+        // result case 1, 2, 3
+        const result = [0, 1, -1];
+        const [pobi, crong] = [
+            [
+                // pobi test case 1, 2, 3
+                [97, 98],
+                [131, 132],
+                [99, 102],
+            ],
+            [
+                // crong test case 1, 2, 3
+                [197, 198],
+                [211, 212],
+                [211, 212],
+            ],
+        ];
+
+        let count = 0;
+
+        result.map((_, i) => (this.solution(pobi[i], crong[i]) === result[i] ? count++ : 0));
+
+        let score = ~~((count / result.length) * 100);
+
+        this.setResult(
+            `
+                테스트 케이스 개수: ${result.length}<br>
+                맞춘 개수: ${count}<br>
+                점수: ${score}점<br>
+            `
+        );
+    };
+}
+
+class TestPr6 {
+    constructor(solution) {
+        this.solution = solution;
+        this.checkScore();
+    }
+
+    setResult = (result) => {
+        this.result = result;
+    };
+
+    printResult = () => {
+        return this.result;
+    };
+
+    /** 점수 확인 */
+    checkScore = () => {
+        // result case 1, 2, 3
+        const result = [0, 1, -1];
+        const [pobi, crong] = [
+            [
+                // pobi test case 1, 2, 3
+                [97, 98],
+                [131, 132],
+                [99, 102],
+            ],
+            [
+                // crong test case 1, 2, 3
+                [197, 198],
+                [211, 212],
+                [211, 212],
+            ],
+        ];
+
+        let count = 0;
+
+        result.map((_, i) => (this.solution(pobi[i], crong[i]) === result[i] ? count++ : 0));
+
+        let score = ~~((count / result.length) * 100);
+
+        this.setResult(
+            `
+                테스트 케이스 개수: ${result.length}<br>
+                맞춘 개수: ${count}<br>
+                점수: ${score}점<br>
+            `
+        );
+    };
+}
+
+export { TestPr1, TestPr2, TestPr3, TestPr4, TestPr5, TestPr6 };
